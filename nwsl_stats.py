efd0ba9b6912691a0d01211972a7ca262115a24c
Eileen Dwyer <eolson@smalltalku.com>
2016-06-05 22:00:44 -0400

    normal mode

--- nwsl_stats.py
+++ nwsl_stats.py
@@ -9,23 +9,22 @@ def welcome():
                     "You can (S)earch or (A)dd a goalie to the database?").lower()
     if welcome == "s":
         search_data()
-#    else:
-#        add_data()
+    if welcome == "a":
+        add_data()
 
 
 def search_data():
-    search = input("Search by (P)layerName | (T)eamName | S(h)ots on Goal | S(a)ves").lower()
+    search = input("Search by (P)layerName, (T)eamName, S(h)ots On Goal, Sa(v)es").lower()
     if search == "p":
         name_search()
     elif search == "t":
         team_search()
     elif search == "h":
-        shotsgoal_search()
-    elif search == "a":
-        saves_search()
+        shots_on_goal_search()
     else:
-        print("Incorrect search entry.")
-        search_data()
+        if search == "v":
+            saves_search()
+            search_data()
 
 
 def name_search():
@@ -44,8 +43,8 @@ def team_search():
     search_data()
 
 
-def shotsgoal_search():
-    cursor.execute("select PlayeName, ShotsonGoals from goalie_stats order by ShotsonGoal DESC ;")
+def shots_on_goal_search():
+    cursor.execute("select PlayerName, ShotsOnGoal from goalie_stats order by ShotsOnGoal DESC ;")
     result = cursor.fetchall()
     print(result)
     search_data()
@@ -57,7 +56,31 @@ def saves_search():
     print(result)
     search_data()
 
+def add_data():
+    PlayerName = input("Enter the player's name: ")
+    TeamName = input("Enter the team name, example, SEA: ")
+    GamesPlayed = input("Enter the number of games played: ")
+    Minutes = input("Enter the number of minutes played, example, 180: ")
+    ShotsOnGoal = input("Enter the number of shots on goal: ")
+    Saves = input("Enter the number of saves: ")
+    GA = input("Enter the number of goals against: ")
+    GAA = input("Enter the average number of goals against: ")
+    SHO = input("Enter the number of shutouts: ")
 
+    cursor.execute("INSERT INTO goalie_stats VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s);",\
+                   (PlayerName, TeamName, GamesPlayed, Minutes, ShotsOnGoal, Saves, GA, GAA, SHO))
+
+    connection.commit()
+    print("Your goalie data has been added to the database.")
+    search_data()
+
+
+add_data()
 welcome()
+
+cursor.close()
+connection.close()
+
+
 
 
